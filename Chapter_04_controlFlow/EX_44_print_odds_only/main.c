11ce3a4f210c5a9c56c6dc1223f69b22e56a3b7e
y.Poursoltani <poursoltani@aut.ac.ir>
2023-11-12 10:59:32 +0330

    examples 44 modified

--- Chapter_04_controlFlow/EX_44_print_odds_only/main.c
+++ Chapter_04_controlFlow/EX_44_print_odds_only/main.c
@@ -3,11 +3,23 @@
 
 int main()
 {
+
+    //Example of continue
     for(int i=1; i<=20; i++){
         if(i%2==0){
             printf("%d\n",i);
         }else{
             continue;
+        }
+    }
+
+
+    //Example of break
+    for(int i=1; i<=20; i++){
+        if(i!=12){
+            printf("%d\n",i);
+        }else{
+            break;
         }
     }
 }
